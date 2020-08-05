a71416b1475dd07513206a59c2f59940455a8c33
RayHan904 <rehan904@yahoo.com>
2020-08-05 06:36:09 -0700

    Updates

---
+++ precache-manifest.cccb1c11ac52b214451834d3d3f01182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a0b53d10be57ef421a9b8ed7a58fe1",
+    "url": "/MyFirstReactApp-RecipeFinder/index.html"
+  },
+  {
+    "revision": "87d7583036d465e05b0c",
+    "url": "/MyFirstReactApp-RecipeFinder/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "858064d371bb5dbad7fd",
+    "url": "/MyFirstReactApp-RecipeFinder/static/js/2.106f2ee7.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/MyFirstReactApp-RecipeFinder/static/js/2.106f2ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d7583036d465e05b0c",
+    "url": "/MyFirstReactApp-RecipeFinder/static/js/main.25ec0bf1.chunk.js"
+  },
+  {
+    "revision": "94215f82bc83c4be18bc",
+    "url": "/MyFirstReactApp-RecipeFinder/static/js/runtime-main.334025ea.js"
+  }
+]);
